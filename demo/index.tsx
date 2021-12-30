09d363458c0fc14b4ea950d74f58eb122cce62cc
weiwang <winoooops@outlook.com>
2021-12-30 16:36:22 +0800

    feature: demo中加入样式

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import * as MyReact from "../src/MyReact";
+import './styles.scss'
+
 // 因为@babel/react-preset中的pragma无法和runtime属性一起使用，造成不手动调用一次babel不自动找到MyReact的问题
 console.log(MyReact)
 const vDOM = (
   <div className="container">
-    <ul>
-      <li>createElement</li>
-      <li>rendering</li>
-      <li>component</li>
-      <li>diff</li>
-      <li>state</li>
+    <ul className="todos" ref="todos">
+      <li className="todo" onClick={() => alert('ongoing')}>createElement</li>
+      <li className="todo" onClick={() => alert('ongoing')} >rendering</li>
+      <li className="todo" onClick={() => alert('ongoing')} >component</li>
+      <li className="todo" onClick={() => alert('ongoing')} >diff</li>
+      <li className="todo" onClick={() => alert('ongoing')} >state</li>
     </ul>
   </div>
 )
@@ -26,5 +28,5 @@ const App = function () {
 
 
 
-const root = document.getElementById('app')
+const root = document.getElementById('app') as HTMLElement
 MyReact.render(<App />, root)
--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,13 @@ module.exports = {
   devtool: "inline-source-map",
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: "babel-loader"
-      // },
       {
         test: /\.tsx?$/,
         use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.scss?$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
